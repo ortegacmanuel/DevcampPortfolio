072233dc923312c05ba11e95a82bce674f0bcd30
Manuel Ortega <manuel@lasindias.coop>
2017-10-08 15:44:26 +0200

    Implemented index and show actions for the API portfolios controller

--- app/controllers/api/portfolios_controller.rb
+++ app/controllers/api/portfolios_controller.rb
@@ -1,2 +1,10 @@
-class PortfoliosController < ApplicationController
+class Api::PortfoliosController < ApplicationController
+  def index
+    render json: Portfolio.all
+  end
+
+  def show
+    portfolio = Portfolio.find(params[:id])
+    render json: portfolio
+  end
 end
