2ddf8551405e651f67f29251bba3498ca02a50dd
Manuel Ortega <manuel@lasindias.coop>
2017-10-08 12:32:59 +0200

    Implemented destroy action for portfolio feature

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -39,6 +39,19 @@ class PortfoliosController < ApplicationController
     end
   end
 
+  def destroy
+    # Perform the lookup
+    @portfolio_item = Portfolio.find(params[:id])
+
+    # destroy/detele the record
+    @portfolio_item.destroy
+
+    # Redirect
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Record was successfully removed.' }
+    end
+  end
+
   private
 
   # Never trust parameters from the scary internet, only allow the white list through.
