88db207bce59c55d95207b0d116949c1079b1e10
Manuel Ortega <manuel@lasindias.coop>
2017-10-08 16:34:18 +0200

    Implemented update and destroy actions for the API portfolios controller

--- app/controllers/api/portfolios_controller.rb
+++ app/controllers/api/portfolios_controller.rb
@@ -26,6 +26,32 @@ class Api::PortfoliosController < ApplicationController
     end
   end
 
+  def update
+    portfolio = Portfolio.find(params[:id])
+    if portfolio.update(portfolio_params)
+      render json: {
+        status: 200,
+        message: "Portfolio successfully updated.",
+        portfolio: portfolio
+      }.to_json
+    else
+      render json: {
+        status: 500,
+        message: "Portfolio could not be updated.",
+        portfolio: portfolio
+      }.to_json
+    end
+  end
+
+  def destroy
+    portfolio = Portfolio.find(params[:id])
+    portfolio.destroy
+    render json: {
+      status: 200,
+      message: "Portfolio successfully deleted."
+    }.to_json
+  end
+
   private
   def portfolio_params
     params.require(:portfolio).permit(
