d665a25d4257ee5f654cb50e187222e323fa023f
Manuel Ortega <manuel@lasindias.coop>
2017-10-08 16:21:53 +0200

    Implemented the create action for portfolios API controller

--- app/controllers/api/portfolios_controller.rb
+++ app/controllers/api/portfolios_controller.rb
@@ -1,4 +1,6 @@
 class Api::PortfoliosController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     render json: Portfolio.all
   end
@@ -6,5 +8,30 @@ class Api::PortfoliosController < ApplicationController
   def show
     portfolio = Portfolio.find(params[:id])
     render json: portfolio
+  end
+
+  def create
+    portfolio = Portfolio.new(portfolio_params)
+    if portfolio.save
+      render json: {
+        status: 200,
+        message: "Portfolio successfully created.",
+        portfolio: portfolio
+      }.to_json
+    else
+      render json: {
+        status: 500,
+        errors: portfolio.errors
+      }.to_json
+    end
+  end
+
+  private
+  def portfolio_params
+    params.require(:portfolio).permit(
+      :title,
+      :subtitle,
+      :body
+    )
   end
 end
