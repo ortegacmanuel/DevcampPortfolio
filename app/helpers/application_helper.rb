2c39382cedb5407a59c2617e751221135111c9f5
Manuel Ortega <manuel@lasindias.coop>
2017-10-24 12:21:00 +0200

    Integrated new gem into rails app for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: 'source-greeting')
     end
   end
+
+  def copyright_generator
+    OrtegacmanuelViewTool::Renderer.copyright 'Manuel Ortega', 'Free Software'
+  end
 end
